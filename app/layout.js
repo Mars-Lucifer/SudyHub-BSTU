758ed6cd3427a5ab21184cc37f11f1f989ec8c24
Mars-Lucifer <nikitarybalko897@gmail.com>
2023-11-23 21:04:09 +0300

    add analit

--- app/layout.js
+++ app/layout.js
@@ -2,24 +2,7 @@ import { Montserrat } from 'next/font/google'
 import './bootstrap-grid.min.css'
 import './bootstrap.min.css'
 import './globals.css'
-
-const mongo = require('mongodb').MongoClient;
-
-mongo.connect(
-    'mongodb://localhost:27017',
-    (err, client) => {
-        if (err) {
-            console.log('Connection error: ', err);
-            throw err;
-        }
-
-        console.log('Connected');
-
-        const db = client.db('main');
-        const posts = db.createCollection('posts');
-    }
-);
-
+import { Analytics } from '@vercel/analytics/react';
 
 const montserrat = Montserrat({
   weight: ['500', '600', '700'],
@@ -30,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={montserrat.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
